bd8c11b268b1f613e61ee76532262c30dd0d25f7
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2022-11-04 23:25:11 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -4,6 +4,6 @@ from keep_alive import keep_alive
 
 keep_alive()
 
-os.system('ffmpeg -re -i https://bit.ly/3kq9SCE -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://origin.playdj.tv/live/3cc0df3e-388d-4b5b-8816-1fa22ecf4bbb')
+os.system('ffmpeg -re -i https://ww1.shikaritv.tk/stsp1hin.php -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://ovsu.mycdn.me/input/720360974_720360974_43_pgi2q3qkha')
 os.system('python3 main.py')
 
