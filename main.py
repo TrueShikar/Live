e50407e8fabf856df357d595b4c00d09f1ef9b06
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2022-11-06 17:30:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from keep_alive import keep_alive
 
 keep_alive()
 
-os.system('ffmpeg -re -i https://ww1.shikaritv.tk/stsp1hin.php -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://ovsu.mycdn.me/input/720360974_720360974_43_pgi2q3qkha')
+os.system('ffmpeg -re -i https://da.gd/grBQl -i  "https://te.legra.ph/file/1f9f9a7e49978b17da41e.jpg" -filter_complex "overlay=10:10" -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://ovsu.mycdn.me/input/720360974_720360974_43_pgi2q3qkha')
 os.system('python3 main1.py')
 
