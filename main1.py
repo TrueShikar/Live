b7586bdade7eb4036bb27622c9c086fc31810916
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2022-11-24 22:13:08 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -4,6 +4,6 @@ from keep_alive import keep_alive
 
 keep_alive()
 
-os.system('ffmpeg -re -i https://ww1.shikaritv.tk/gocast.php?v=fox503 -i  "https://bold-silence-0eed.shikari.workers.dev/0:/20221108_134525.png" -filter_complex "overlay=1000:20" -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmps://rtmp-global.cloud.vimeo.com:443/live/ab51f5ea-47e7-4d0c-bcfe-b5b92594e8cf')
+os.system('ffmpeg -re -i https://mx.techyshikari.in/Bb24x7.m3u8 -i  "https://bold-silence-0eed.shikari.workers.dev/0:/20221108_134525.png" -filter_complex "overlay=1000:20" -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmps://rtmp-global.cloud.vimeo.com:443/live/d7cd6e68-61cf-4e75-844a-5d66972e7fc4')
 os.system('python3 main.py')
 
